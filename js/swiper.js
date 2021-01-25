3439cb2fe44febef3a6f3e23eee385adf691e565
Kevin <kevinbulfanisay1@gmail.com>
2021-01-25 16:23:11 +0300

    initial commit

---
+++ js/swiper.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    effect: 'fade',
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
