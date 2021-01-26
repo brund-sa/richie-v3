4dbb305e69d3b67ab9ff13270db7dcb82e096132
Kevin <kevinbulfanisay1@gmail.com>
2021-01-26 11:50:51 +0300

    test contact

--- js/swiper.js
+++ js/swiper.js
@@ -10,7 +10,11 @@ var swiper = new Swiper('.swiper-container', {
     },
   });
 
+/* =====================================================
 
+                        MAP AREA 
+
+=======================================================*/
 // Initialize and add the map
 function initMap() {
 var iconBase = '../images/icons/map-marker.png';
@@ -291,3 +295,64 @@ const marker = new google.maps.Marker({
     icon: iconBase
 });
 }
+
+
+/* =====================================================
+
+                        
+                    Contact form 
+
+=======================================================*/
+$("#send_form").on('submit', function(){
+    var first_name = $("#first_name").val().trim();
+    var last_name = $("#last_name").val().trim();
+    var email = $("#email").val().trim();
+    var number = $("#number").val().trim();
+    var message = $("#message").val().trim();
+
+    $.ajax({
+        url: '../mail.php',
+        type: 'POST',
+        cache: false,
+        data: {'first_name': first_name, 'last_name': last_name, 'email': email, 'number': number, 'message': message},
+        dataType: 'html',
+        beforeSend: function() {
+            $("#send").addClass("js-active");
+        },
+        success: function(data) {
+            if(!data || data != "Good"){
+                $("#m_err").addClass("js-active");
+                $(".form-box").addClass("js-active");
+                $("#send").removeClass("js-active");
+            }
+
+            else {
+                $("#m_sent").addClass("js-active");
+                $(".form-box").addClass("js-active");
+                $("#send").removeClass("js-active");
+                $("#send_form").trigger("reset");
+            }
+            $('.js-popup-close').click(function() {
+                $(this).parents('.js-popup-fade').removeClass("js-active");
+                $('.form-box').removeClass("js-active");
+                return false;
+            });
+
+            $(document).keydown(function(e) {
+                if (e.keyCode === 27) {
+                    e.stopPropagation();
+                    $('.js-popup-fade').removeClass("js-active");
+                    $('.form-box').removeClass("js-active");
+                }
+            });
+
+            $('.js-popup-fade').click(function(e) {
+                    if ($(e.target).closest('.js-popup').length == 0) {
+                    $('.js-popup-fade').removeClass("js-active");
+                    $('.form-box').removeClass("js-active");
+                }
+            });
+        },
+    })
+    return false;
+});
--- mail.php
+++ mail.php
@@ -1,10 +1,11 @@
 <?php
 $to = "kevin@brund.run";
-$first_name = $_POST['name'];
+$name = $_POST['name'];
 $email = $_POST['email'];
+$number = $_POST['number'];
 $message = $_POST['message'];
 
-	$message = "First name: $name\nEmail: $email\nMessage: $message";
+	$message = "First name: $name\nEmail: $email\nNumber: $number\nMessage: $message";
 	$subject = "=?utf-8?B?".base64_encode("message from the site")."?=";
 	$headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/plain; charset=utf-8\r\n";
 
