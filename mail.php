283ef754e9c903997932cd4edea9338824b5064b
Kevin <kevinbulfanisay1@gmail.com>
2021-01-26 10:49:13 +0300

    contact-form.php

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+$to = "kevin@brund.run";
+$first_name = $_POST['first_name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+	$message = "First name: $first_name\nEmail: $email\nMessage: $message";
+	$subject = "=?utf-8?B?".base64_encode("message from the site")."?=";
+	$headers = "From: $email\r\nReply-to: $email\r\nContent-type: text/plain; charset=utf-8\r\n";
+
+	if(mail($to, $subject, $message, $headers))
+		echo "Good";
+	else
+		echo "Error";
+
+?>
