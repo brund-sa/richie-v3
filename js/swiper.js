0e4ec4a001593b8cbb6f1120f124980406f937e6
Kevin <kevinbulfanisay1@gmail.com>
2021-02-13 10:04:04 +0300

    projects merge

--- js/swiper.js
+++ js/swiper.js
@@ -298,6 +298,53 @@ const marker = new google.maps.Marker({
     icon: iconBase
 });
 }
+/* =====================================================
+
+                        LEGAL ACTION
+
+=======================================================*/
+var message="Please do not download the images.";
+        
+        function clickIE6(){
+        
+        if (event.button==2){
+        
+        
+        return false;
+        
+        }
+        
+        }
+        
+        function clickNS4(e){
+        
+        if (document.layers||document.getElementById&&!document.all){
+        
+        if (e.which==2||e.which==3){
+        
+        return false;
+        
+        }
+        
+        }
+        
+        }
+        
+        if (document.layers){
+        
+        document.captureEvents(Event.MOUSEDOWN);
+        
+        document.onmousedown=clickNS4;
+        
+        }
+        
+        else if (document.all&&!document.getElementById){
+        
+        document.onmousedown=clickIE6;
+        
+        }
+        
+        document.oncontextmenu=new Function("return false")
 
 
 
